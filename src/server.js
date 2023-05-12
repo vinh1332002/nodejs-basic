d8a5f7743e0644b5fba87c5eea811c31efbfb83e
vinh <vinh1332002@gmail.com>
2023-05-12 16:32:39 +0700

    commit static files

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
+require('dotenv').config()
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+configViewEngine(app);
+
+app.get("/", (req, res) => {
+  res.render("test/index.ejs");
+});
+
+app.get("/ab", (req, res) => {
+  res.send("Cha hieu sao Vinh ");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
